294518f443be7d83e8aaaae2748d81cd1200331b
santannalucas <santanna.lucas@gmail.com>
2023-02-07 17:48:52 +1100

    file reading and test

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,6 @@
 class PeopleController < ApplicationController
 
+  require 'csv'
   require 'will_paginate/array'
   def index
 
@@ -7,7 +8,15 @@ class PeopleController < ApplicationController
 
   def upload_file
     if params[:file]
-      flash = "Wow"
+      file = File.open(params[:file])
+      begin
+        CSV.foreach(file.path).each do |row|
+        Rails.logger.info(row)
+        end
+      rescue
+        Rails.logger.info('Could not read from file, please upload CSV file only.')
+      end
+      Rails.logger.info("Wow")
       redirect_to people_path
     end
   end
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,5 @@ class Person < ActiveRecord::Base
   has_many :locations
   has_many :affiliations
 
+  validates_uniqueness_of :first_name, :last_name
 end

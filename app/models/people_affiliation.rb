46e1ac40be9b23263f6e50d36fe19c37aa810a6c
santannalucas <santanna.lucas@gmail.com>
2023-02-08 21:37:07 +1100

    test units and pre final review

--- app/models/people_affiliation.rb
+++
@@ -1,6 +0,0 @@
-class PersonAffiliation < ActiveRecord::Base
-
-  belongs_to :person
-  belongs_to :affiliation
-
-end
--- app/models/people_location.rb
+++
@@ -1,6 +0,0 @@
-class PersonLocation < ActiveRecord::Base
-
-  belongs_to :person
-  belongs_to :location
-
-end
